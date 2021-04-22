f5f5dc1a57494662ac6b0ae9f2c8cb8d282f5080
sukantahui <huisukanta77@gmail.com>
2021-04-22 14:47:49 +0530

    first commit

---
+++ app/Models/Car.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Car extends Eloquent
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'cars';
+
+    protected $fillable = [
+        'carcompany', 'model','price'
+    ];
+}
